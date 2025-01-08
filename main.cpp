a5c88f90880027a1e3176faaf90b87a3922f7926
Sairanjan-Subudhi <ranjansai1214@gmail.com>
2025-01-08 14:46:11 +0530

    added new lines to main.cpp

--- main.cpp
+++ main.cpp
@@ -3,5 +3,16 @@ using namespace std;
 
 int main() {
     cout << "Hello, World!" << endl;
+
+    string name;
+    cout << "Enter your name: ";
+    cin >> name;
+
+    int age;
+    cout << "Enter your age: ";
+    cin >> age;
+
+    cout << "Hello, " << name << "! You are " << age << " years old." << endl;
+
     return 0;
 }
